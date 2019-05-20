d0678c4b64b3c1ef0a4e29c479ffc5e59e765ac3
nintendavid26 <nintendavid26@aol.com>
2019-05-20 14:15:05 -0400

    added start menu

---
+++ Assets/_Scripts/Menus.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menus : MonoBehaviour
+{
+    public void LoadScene(int index){
+        UnityEngine.SceneManagement.SceneManager.LoadScene(index);
+    }
+}
